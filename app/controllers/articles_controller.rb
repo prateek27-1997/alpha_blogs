0957b809a11a33d71ad16c76da0968a82bef74a2
Rails udemy course <Railsudemycourse@gmail.com>
2020-03-08 15:38:03 +0530

    edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,20 @@ class ArticlesController < ApplicationController
         render 'new'
     end
 end
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Articles was sucessfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+end
+
 
     def show
         @article = Article.find(params[:id])
